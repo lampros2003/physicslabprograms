013dba374a5ed32b75b862afa6a38b4b2d4d2d8f
lampros2003 <46668710+lampros2003@users.noreply.github.com>
2022-03-26 20:02:44 +0200

    add and subtract

--- scicalclib.py
+++ scicalclib.py
@@ -1,10 +1,12 @@
+from cgi import print_environ
 from copy import error
+from email.mime import message
 import math
 import sympy
-
-class measurement(float):
-    def round_it(x, sig):
+def round_it(x, sig):
         return round(x, sig-int(math.floor(math.log10(abs(x))))-1)
+class measurement(float):
+    
     def limittosigfig(self):
         sfig = (str(self.value)+(self.impd*"0"))[:self.impd+1]
         return sfig
@@ -24,13 +26,37 @@ class measurement(float):
         value = super().__mul__(__x)
         importantdigits = min(self.impd,__x.impd)
         error = self.error
+        value = round_it(value, importantdigits)
 
         return (value,importantdigits,error,self.unit+"*"+self.unit)
 
+    def __add__(self, other) :
+        self.impd = min(self.impd,other.impd)
+        other.impd = self.impd
+        self.value = float(self.limittosigfig()) 
+        other.value = float(other.limittosigfig()) 
+        outvalue =super().__add__(other)
+        if len(str(outvalue))> self.impd:
+            sigfig = len(str(outvalue))
+        else :
+            sigfig = self.impd
+        out = measurement(outvalue,sigfig,self.error + other.error,self.unit)
+        return out
+    def __sub__(self, other: float) :
+        self.impd = min(self.impd,other.impd)
+        other.impd = self.impd
+        self.value = float(self.limittosigfig()) 
+        other.value = float(other.limittosigfig()) 
+        outvalue =super().__sub__(other)
+        
+        out = measurement(outvalue,self.impd,self.error + other.error,self.unit)
+        return out
     
         
-a = measurement(14.199,6,0.001,"cm")*measurement(14.1,6,0.001,"cm")
-print(measurement(a[0],a[1],a[2],a[3]))
+a = measurement(14.199,7,0.001,"cm")-measurement(14.1,7,0.001,"cm")
+print(a)
+
+
     
 
 
